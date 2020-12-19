b06d40181ff106a2cb28aa357c2bb55bdc55f0aa
jessicabarbieri <jessicabarbieri10@gmail.com>
2020-12-19 15:58:12 -0300

    dbcontext e dbset

---
+++ ProjetoEcommerce.Repositorio/Contexto/ProjetoContexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoEcommerce.Dominio.Entidades;
+using ProjetoEcommerce.Dominio.ObjetodeValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoEcommerce.Repositorio.Contexto
+{
+    public class ProjetoContexto: DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItensPedidos { get; set; }
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+
+    }
+}
